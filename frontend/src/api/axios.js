5188207e36b28556d2a48709f091bacc39d0681c
parthasarathiAVS <sarathi1881@gmail.com>
2026-07-24 23:19:33 -0700

    Prepare project for fresh deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://online-election-system-9sx9.onrender.com'
+  baseURL: 'https://online-election-system-9sx9.onrender.com/api/auth/admin/login'
 });
 
 api.interceptors.request.use((config) => {
